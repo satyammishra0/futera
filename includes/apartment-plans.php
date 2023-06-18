9528c22343d29b5a063c25f64260c8ad6e6d0e0e
satyam mishra <satyam.growupnext@gmail.com>
2023-06-18 11:49:18 +0530

    new slider added in home page

---
+++ includes/apartment-plans.php
@@ -0,0 +1,316 @@
+<style>
+    swiper-container {
+        width: 100%;
+        height: 100%;
+    }
+
+    swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+    }
+
+    swiper-container {
+        width: 100%;
+        height: 300px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    swiper-slide {
+        background-size: cover;
+        background-position: center;
+    }
+
+    .mySwiper {
+        height: 80%;
+        width: 100%;
+    }
+
+    .mySwiper2 {
+        height: 20%;
+        box-sizing: border-box;
+        padding: 10px 0;
+    }
+
+    .mySwiper2 swiper-slide {
+        width: 25%;
+        height: 100%;
+        opacity: 0.4;
+    }
+
+    .mySwiper2 .swiper-slide-thumb-active {
+        opacity: 1;
+    }
+
+    swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+    }
+
+    /* Arrows styling */
+    .swiper-button-next,
+    .swiper-button-prev {
+        color: var(--dark-orange) !important;
+    }
+
+    .swiper-parent-overlay {
+        position: relative;
+    }
+
+    .swiper-overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: #0000008f;
+        color: var(--white);
+        font-size: 25px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+
+<swiper-container style="--swiper-navigation-color: var(--dark-orange) !important; --swiper-pagination-color: #fff" class="mySwiper" thumbs-swiper=".mySwiper2" space-between="10" navigation="true">
+    <swiper-slide>
+        <div class="home_fourth-section-grid utility_grid">
+            <div class="home_fourth-section-grid-content">
+                <h4>Monterey</h4>
+                <div class="utility_flex-center-between">
+                    <p>Home</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Room</p>
+                    <p>3</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Windows</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Total Sq. Km</p>
+                    <p>120</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Parking</p>
+                    <p>YES</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Price</p>
+                    <p><span style="color:var(--greenish-blue);font-weight:bold;">$2500</span></p>
+                </div>
+
+                <button class="button-ani">
+                    <a href="../.cpanel.yml" download>Download Brochure</a>
+                </button>
+
+            </div>
+            <div class="home_fourth-section-grid-img">
+                <img src="../assets/images/apartement-plan-1.webp" alt="">
+            </div>
+        </div>
+    </swiper-slide>
+    <swiper-slide>
+        <div class="home_fourth-section-grid utility_grid">
+            <div class="home_fourth-section-grid-content">
+                <h4>Monterey</h4>
+                <div class="utility_flex-center-between">
+                    <p>Home</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Room</p>
+                    <p>3</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Windows</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Total Sq. Km</p>
+                    <p>120</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Parking</p>
+                    <p>YES</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Price</p>
+                    <p><span style="color:var(--greenish-blue);font-weight:bold;">$2500</span></p>
+                </div>
+
+                <button class="button-ani">
+                    <a href="../.cpanel.yml" download>Download Brochure</a>
+                </button>
+
+            </div>
+            <div class="home_fourth-section-grid-img">
+                <img src="../assets/images/apartement-plan-1.webp" alt="">
+            </div>
+        </div>
+    </swiper-slide>
+    <swiper-slide>
+        <div class="home_fourth-section-grid utility_grid">
+            <div class="home_fourth-section-grid-content">
+                <h4>Monterey</h4>
+                <div class="utility_flex-center-between">
+                    <p>Home</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Room</p>
+                    <p>3</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Windows</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Total Sq. Km</p>
+                    <p>120</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Parking</p>
+                    <p>YES</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Price</p>
+                    <p><span style="color:var(--greenish-blue);font-weight:bold;">$2500</span></p>
+                </div>
+
+                <button class="button-ani">
+                    <a href="../.cpanel.yml" download>Download Brochure</a>
+                </button>
+
+            </div>
+            <div class="home_fourth-section-grid-img">
+                <img src="../assets/images/apartement-plan-1.webp" alt="">
+            </div>
+        </div>
+    </swiper-slide><swiper-slide>
+        <div class="home_fourth-section-grid utility_grid">
+            <div class="home_fourth-section-grid-content">
+                <h4>Monterey</h4>
+                <div class="utility_flex-center-between">
+                    <p>Home</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Room</p>
+                    <p>3</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Windows</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Total Sq. Km</p>
+                    <p>120</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Parking</p>
+                    <p>YES</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Price</p>
+                    <p><span style="color:var(--greenish-blue);font-weight:bold;">$2500</span></p>
+                </div>
+
+                <button class="button-ani">
+                    <a href="../.cpanel.yml" download>Download Brochure</a>
+                </button>
+
+            </div>
+            <div class="home_fourth-section-grid-img">
+                <img src="../assets/images/apartement-plan-1.webp" alt="">
+            </div>
+        </div>
+    </swiper-slide><swiper-slide>
+        <div class="home_fourth-section-grid utility_grid">
+            <div class="home_fourth-section-grid-content">
+                <h4>Monterey</h4>
+                <div class="utility_flex-center-between">
+                    <p>Home</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Room</p>
+                    <p>3</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Windows</p>
+                    <p>6</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Total Sq. Km</p>
+                    <p>120</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Parking</p>
+                    <p>YES</p>
+                </div>
+                <div class="utility_flex-center-between">
+                    <p>Price</p>
+                    <p><span style="color:var(--greenish-blue);font-weight:bold;">$2500</span></p>
+                </div>
+
+                <button class="button-ani">
+                    <a href="../.cpanel.yml" download>Download Brochure</a>
+                </button>
+
+            </div>
+            <div class="home_fourth-section-grid-img">
+                <img src="../assets/images/apartement-plan-1.webp" alt="">
+            </div>
+        </div>
+    </swiper-slide>
+</swiper-container>
+
+<swiper-container class="mySwiper2" space-between="10" slides-per-view="4" free-mode="true" watch-slides-progress="true">
+    <swiper-slide class="swiper-parent-overlay">
+        <div class="swiper-overlay">
+            <h2>Monterey</h2>
+        </div>
+        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+    </swiper-slide>
+    <swiper-slide class="swiper-parent-overlay">
+        <div class="swiper-overlay">
+            <h2>Vista</h2>
+        </div>
+        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+    </swiper-slide>
+    <swiper-slide class="swiper-parent-overlay">
+        <div class="swiper-overlay">
+            <h2>Pent House</h2>
+        </div>
+        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+    </swiper-slide>
+    <swiper-slide class="swiper-parent-overlay">
+        <div class="swiper-overlay">
+            <h2>Sea Breeze</h2>
+        </div>
+        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+    </swiper-slide>
+    <swiper-slide class="swiper-parent-overlay">
+        <div class="swiper-overlay">
+            <h2>Sunrise</h2>
+        </div>
+        <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+    </swiper-slide>
+</swiper-container>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
