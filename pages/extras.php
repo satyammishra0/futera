5c4eeac0414f8e77404e53b5e4ff85449290adf8
satyam mishra <satyam.growupnext@gmail.com>
2023-06-27 19:12:07 +0530

    slider edits done

---
+++ pages/extras.php
@@ -0,0 +1,54 @@
+<!-- Deleted Images of the home and about slider which were repearted -->
+
+<!-- <div class="slide-box slide-box4">
+    <div class="dark">
+        <div class="play">
+            <div class="img">
+                <img id="icon-1" src="../assets/images/icons8-play-100.png" alt="">
+                <img id="icon-2" src="../assets/images/icons8-play-101.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="video-page"> -->
+<!-- <iframe class="responsive-iframe" width="560" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+
+<!-- <video autoplay muted controls loop id="vid2" class="responsive-iframe">
+            <source src="../assets/videos/video-1.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
+<div class="slide-box slide-box3">
+    <div class="dark">
+        <div class="play">
+            <div class="img">
+                <img id="icon-1" src="../assets/images/icons8-play-100.png" alt="">
+                <img id="icon-2" src="../assets/images/icons8-play-101.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="video-page"> -->
+
+<!-- <iframe class="responsive-iframe" width="560" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+<!-- 
+        <video autoplay muted controls loop id="vid2" class="responsive-iframe">
+            <source src="../assets/videos/video-1.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
+<div class="slide-box slide-box4">
+    <div class="dark">
+        <div class="play">
+            <div class="img">
+                <img id="icon-1" src="../assets/images/icons8-play-100.png" alt="">
+                <img id="icon-2" src="../assets/images/icons8-play-101.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="video-page"> -->
+<!-- <iframe class="responsive-iframe" width="560" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+
+<!-- <video autoplay muted controls loop id="vid2" class="responsive-iframe">
+            <source src="../assets/videos/video-1.mp4" type="video/mp4">
+        </video>
+    </div>
+</div> -->
